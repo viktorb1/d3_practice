7f3746910d3e8ef8389c0f578e70dccc5b08077d
Viktor Babchanik <vbabchanik@ucdavis.edu>
2023-07-02 23:22:46 -0700

    Add more charts

---
+++ bar_chart/app.js
@@ -0,0 +1,37 @@
+async function draw() {
+  // Data
+  const dataset = await d3.csv('data.csv', (d) => {
+    d3.autoType(d)
+    return d
+  })
+
+  console.log(dataset)
+
+  // Dimensions
+  let dimensions = {
+    width: 1000,
+    height: 600,
+    margins: 20,
+  };
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+
+  // Draw Image
+  const svg = d3.select('#chart')
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height)
+
+  const ctr = svg.append("g")
+    .attr(
+      "transform",
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    )
+
+  // Scales
+  const stackGenerator = d3.stack()
+      .keys()
+}
+
+draw()
