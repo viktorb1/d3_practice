36bff2fc9169473df52d6eda5d570aa23f7211a2
Viktor Babchanik <vbabchanik@ucdavis.edu>
2023-07-03 02:55:20 -0700

    Add bar chart

--- bar_chart/app.js
+++ bar_chart/app.js
@@ -1,11 +1,12 @@
 async function draw() {
   // Data
-  const dataset = await d3.csv('data.csv', (d) => {
+  const dataset = await d3.csv('data.csv', (d, index, columns) => {
     d3.autoType(d)
+    d.total = d3.sum(columns, (c) => d[c])
     return d
   })
 
-  console.log(dataset)
+  dataset.sort((a,b) => b.total - a.total)
 
   // Dimensions
   let dimensions = {
@@ -31,7 +32,61 @@ async function draw() {
 
   // Scales
   const stackGenerator = d3.stack()
-      .keys()
+      .keys(dataset.columns.slice(1))
+
+    const stackData = stackGenerator(dataset).map((ageGroup) => {
+      ageGroup.forEach((state) => {
+        state.key = ageGroup.key
+      })
+      return ageGroup
+    })
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(stackData, (ag) => {
+        return d3.max(ag, state => state[1])
+      })])
+      .rangeRound([dimensions.ctrHeight, dimensions.margins])
+    console.log(stackData)
+    const xScale = d3.scaleBand()
+      .domain(dataset.map(state => state.name))
+      .range([dimensions.margins, dimensions.ctrWidth])
+      .paddingInner(0.1)
+      .paddingOuter(0.1)
+    
+    const colorScale = d3.scaleOrdinal()
+    .domain(stackData.map(d => d.key))
+    .range(d3.schemeSpectral[stackData.length])
+    .unknown('#ccc')
+    // Draw Bars
+    const ageGroups = ctr.append('g')
+      .classed('age-groups', true)
+      .selectAll('g')
+      .data(stackData)
+      .join('g')
+      .attr('fill', d => colorScale(d.key))
+
+
+    ageGroups.selectAll('rect')
+      .data(d => d)
+      .join('rect')
+      .attr('x', d => xScale(d.data.name))
+      .attr('y', d => yScale(d[1]))
+      .attr('width', xScale.bandwidth())
+      .attr('height', d=> yScale(d[0]) - yScale(d[1]))
+
+    
+    // Draw Axes
+    const xAxis = d3.axisBottom(xScale)
+    .tickSizeOuter(0)
+    const yAxis = d3.axisLeft(yScale)
+    .ticks(null, 's')
+
+    ctr.append('g')
+      .attr('transform', `translate(0, ${dimensions.ctrHeight})`)
+      .call(xAxis)
+    
+    ctr.append('g')
+      .attr('transform', `translate(${dimensions.margins}, 0)`)
+      .call(yAxis)
 }
 
 draw()
